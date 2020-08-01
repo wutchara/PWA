0287efd3b043271789b2aaa9d057f80f0135cd30
Wutchara Rachadach <wutchara.rachadach@refinitiv.com>
2020-08-01 22:21:08 +0700

    add featching in service worker

--- course-project/public/sw.js
+++ course-project/public/sw.js
@@ -5,3 +5,8 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', (event) => {
     console.log('[Service Worker] Activating Service Worker ....', event);
 });
+
+self.addEventListener('fetch', (event) => {
+    console.log('[Serview Working] Fetching somethind ...', event);
+    event.respondWith(fetch(event.request));
+});
