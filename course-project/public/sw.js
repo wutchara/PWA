fb190bc519d90095a30413633650ead9aa3faf4b
wutchara <SSTMUHAM@gmail.com>
2020-08-02 13:53:12 +0700

    refactor: service worker tacking

--- course-project/public/sw.js
+++ course-project/public/sw.js
@@ -1,12 +1,14 @@
+const appName = '[Service Worker]';
+
 self.addEventListener('install', (event) => {
-    console.log('[Service Worker] Installing Service Worker ....', event);
+    console.log(appName+' Installing Service Worker ....', event);
 });
 
 self.addEventListener('activate', (event) => {
-    console.log('[Service Worker] Activating Service Worker ....', event);
+    console.log(appName+' Activating Service Worker ....', event);
 });
 
 self.addEventListener('fetch', (event) => {
-    console.log('[Serview Working] Fetching somethind ...', event);
+    console.log(appName+' Fetching somethind ...', event);
     event.respondWith(fetch(event.request));
 });
