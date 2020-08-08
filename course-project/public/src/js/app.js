19d4782f1e63a28b29378a30ecf077f2c5c9143d
wutchara <SSTMUHAM@gmail.com>
2020-08-08 20:04:00 +0700

    Modify app

--- course-project/public/src/js/app.js
+++ course-project/public/src/js/app.js
@@ -1,38 +1,23 @@
 var deferredPrompt;
 
-// support legacy browser
-if(!window.Promise){
-    window.Promise = Promise;
+if (!window.Promise) {
+  window.Promise = Promise;
 }
 
-if('serviceWorker' in navigator) {
-    navigator.serviceWorker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
     .register('/sw.js')
-    .then(() => {
-        console.log('Service worker registered!!!');
-    }).catch((err) => {
-        console.log('error register service worker', err);
+    .then(function () {
+      console.log('Service worker registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
     });
 }
 
-window.addEventListener('beforeinstallprompt', (event) => {
-    console.log('beforeinstallprompt fired');
-    event.preventDefault();
-    deferredPrompt = event;
-    return false;
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
 });
-
-const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('This is executed once the timer is done!!');
-    }, 3000);
-});
-
-promise.then((next) => {
-    console.log('next', next);
-    return next;
-}).then((nextText) => {
-    console.log('nextText', nextText);
-});
-
-console.log('This is executed right after setTimeout()');
