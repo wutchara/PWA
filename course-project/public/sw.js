c3b044aaad5b1f7e7454ebe87004c312caae3337
wutchara <SSTMUHAM@gmail.com>
2020-08-08 20:36:12 +0700

    add static pre-caching

--- course-project/public/sw.js
+++ course-project/public/sw.js
@@ -2,13 +2,47 @@ const appName = '[Service Worker]';
 
 self.addEventListener('install', (event) => {
     console.log(appName+' Installing Service Worker ....', event);
+    event.waitUntil(
+        caches.open('static')
+        .then((cache) => {
+            console.log(appName+' Pre-caching App shell.');
+            cache.addAll([
+                '/',
+                '/index.html',
+                '/src/js/app.js',
+                '/src/js/feed.js',
+                '/src/js/promise.js',
+                '/src/js/fetch.js',
+                '/src/js/material.min.js',
+                '/src/css/app.css',
+                '/src/css/feed.css',
+                '/src/images/main-image.jpg',
+                'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                'https://fonts.googleapis.com/icon?family=Material+Icons',
+                'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+                
+            ]);
+        }).catch((err) => {
+            console.log('Error caches static', err);
+        })
+    )
 });
 
 self.addEventListener('activate', (event) => {
     console.log(appName+' Activating Service Worker ....', event);
+    return self.ClientRectList.claim();
 });
 
 self.addEventListener('fetch', (event) => {
     console.log(appName+' Fetching something ...', event);
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+        caches.match(event.request)
+        .then((response) => {
+            if(response){
+                return response;
+            }else{
+                return fetch(event.request);
+            }
+        })
+    );
 });
